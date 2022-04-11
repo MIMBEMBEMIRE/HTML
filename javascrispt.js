7e8446e65dfacf07dab6823054fa581c7cb455b2
Mireille <miramimbembe1991@gmail.com>
2022-04-11 10:44:10 +0530

    javascript

---
+++ javascrispt.js
@@ -0,0 +1,14 @@
+alert("cousou");
+console.log("hello world");
+ var n = "welcome";
+ console.log(n)
+ var re ="tu me marier";
+ console.log(re);
+ var mama="la paix du coeur " + re
+ console.log(mama);
+ var object ={
+     nom:"mireille",
+     age:24,
+     post:"luwaba",
+ }
+ console.log(object.age);
