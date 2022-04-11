bd6b9766a181184666ef1abb250b53528640236c
Mireille <miramimbembe1991@gmail.com>
2022-04-11 16:08:45 +0530

    style javacript

--- javascrispt.js
+++ javascrispt.js
@@ -1,45 +1,6 @@
-alert("cousou");
-console.log("hello world");
- var n = "welcome";
- console.log(n)
- var re ="tu me marier";
- console.log(re);
- var mama="la paix du coeur " + re
- console.log(mama);
- var object ={
-     nom:"mireille",
-     age:24,
-     post:"luwaba",
- }
- console.log(object.age);
- //operator
- let total =2;
- let x=4;
- x++;
- total=x
- console.log(total);
- function nom(){
-     console.log("hello");
-     console.log("how are you;")
- }
- nom();
- const noms =() =>{
-     console.log("how are you?");
-     console.log("Imfine")
- };
- noms();
-
- //function avec le paramettre
-
- function cal(x,y){
-     return(x+y);
- }
- cal(2+3);
 
 
-// une function qui se joue toute seul
+var app =document.querySelector("h1").style.background="red";
+var clickevent =document.querySelector(".click-event").style.borderRadius="150px";
 
-(function nom1(){
-    console.log("Enter your variaable")
-})();
- 
+
