4d11452e45c83029acddfea8149423c1de422404
Mireille <miramimbembe1991@gmail.com>
2022-04-11 17:08:31 +0530

    javascript rendre le p visible

--- javascrispt.js
+++ javascrispt.js
@@ -1,11 +1,25 @@
 
 
 var app =document.querySelector("h1").style.background="red";
+var bnt1 = document.getElementById("btn-1");
+var bnt2 = document.getElementById("btn-2");
+var answer = document.querySelector("p");
+
+
 var clika =document.querySelector(".click-event");
 clika.style.borderRadius = "150px";
 clika.addEventListener("click", () => {
 clika.classList.add("questio1");
+
 });
 
+bnt1.addEventListener("click", () => {
+answer.style.visibility="visible";
+});
+
+bnt2.addEventListener("click", () =>{
+
+    console.log("click!");
+});
 
 
