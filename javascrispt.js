1690ffd66c2742111d8e97d44725fde2578443aa
Mireille <miramimbembe1991@gmail.com>
2022-04-11 16:40:41 +0530

    addingclass in javascript

--- javascrispt.js
+++ javascrispt.js
@@ -4,8 +4,8 @@ var app =document.querySelector("h1").style.background="red";
 var clika =document.querySelector(".click-event");
 clika.style.borderRadius = "150px";
 clika.addEventListener("click", () => {
-    clika.style.background="blue";
-    console.log("click");
-})
+clika.classList.add("questio1");
+});
+
 
 
