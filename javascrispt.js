c055b35f959caaf1b69470fcfc74db5243dd62f5
Mireille <miramimbembe1991@gmail.com>
2022-04-11 16:30:17 +0530

    click even

--- javascrispt.js
+++ javascrispt.js
@@ -1,6 +1,11 @@
 
 
 var app =document.querySelector("h1").style.background="red";
-var clickevent =document.querySelector(".click-event").style.borderRadius="150px";
+var clika =document.querySelector(".click-event");
+clika.style.borderRadius = "150px";
+clika.addEventListener("click", () => {
+    clika.style.background="blue";
+    console.log("click");
+})
 
 
