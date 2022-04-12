8e29fa56af93fd63cf8269e5ed50e6c8aaf1f68c
Mireille <miramimbembe1991@gmail.com>
2022-04-12 14:16:32 +0530

    add a condition to text box to have a date else alert

--- javascrispt.js
+++ javascrispt.js
@@ -20,7 +20,32 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-50px";
   }
 }
+//form
+const inputname=document.querySelector(input[type="text"]);
+const select =document.querySelector("select");
+let psendo="";
+let language ="";
+const form = document.querySelector("form");
 
+
+inputname.addEventListener("input",()=>{
+pseudo = e.target.value;
+});
+select.addEventListener("input", (e) => {
+    console.log(e.target.value);
+});
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    if(cgv.checked){
+document.querySelector("form > div").innerHTML=
+        "<h3>{psendo}</h3>"
+        "<h4>language:${language}</h4>"
+}else{
+alert("veuille selection GBS");
+    }
+
+
+});
 
 
 
