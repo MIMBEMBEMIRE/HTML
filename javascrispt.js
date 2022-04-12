a5fe3627f0e62c6e6d36df639db66c8ebf366928
Mireille <miramimbembe1991@gmail.com>
2022-04-12 09:02:55 +0530

    scroll bar

--- javascrispt.js
+++ javascrispt.js
@@ -1,67 +1,25 @@
 
 
-var app =document.querySelector("h1").style.background="red";
-var bnt1 = document.getElementById("btn-1");
-var bnt2 = document.getElementById("btn-2");
-var answer = document.querySelector("p");
-
-
-var clika =document.querySelector(".click-event");
-clika.style.borderRadius = "150px";
-clika.addEventListener("click", () => {
-clika.classList.add("questio1");
-
-});
-
-bnt1.addEventListener("click", () => {
-answer.classList.add("showreponse");
-});
-
-bnt2.addEventListener("click", () =>{
-
-   answer.style.visibility ="visibible";
-   answer.style.background ="black";
-});
 //---------------------------------------------
 
-const mouse1 = document.querySelector(".mousemove");
-window.addEventListener("mousemove", (e) =>{
-    mouse1.style.left =e.pageX + "px";
-    mouse1.style.top =e.pageY + "px";
-
-});
-window.addEventListener("mousedown",() =>{
-    mouse1.style.transform ="scale(2) translate(-25%,-25%)";
-});
-
-window.addEventListener("mouseup",() =>{
-    mouse1.style.transform ="scale(2) translate(-50%,-50%)";
-    mouse1.style.border = "2px solid teal"
-});
-
-clika.addEventListener("mouseup",() =>{
-clika.style.background = "rgba(0,0,0,0)";
-});
-clika.addEventListener("mouseout",() =>{
-    clika.style.background = "yellow";
-    });
 
 
-    //keypretById("key");
 
-const keypress1 = document.querySelector(".keypress1");
-const key = document.getElementById("key");
- document.addEventListener("keypress", (e) => {
 
-key.textContent = e.key;
 
-if(e.key==="j"){
-    keypress1.style.background="blue";
-}else if(e.key==="u"){
-    keypress1.style.background ="red";
+//--comment cree un son en javascript a rre
+//scroll event
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    if(window.scrollY > 120){
+        nav.style.top = 0;
+
+}else{
+    nav.style.top ="-50px;";
 }
-
 });
+
 
 
 
