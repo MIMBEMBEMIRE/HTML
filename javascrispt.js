6bb73ce83c9f7ed328f562a703985c27b633059b
Mireille <miramimbembe1991@gmail.com>
2022-04-12 09:18:45 +0530

    scroll bar from w3school

--- javascrispt.js
+++ javascrispt.js
@@ -9,16 +9,18 @@
 
 //--comment cree un son en javascript a rre
 //scroll event
-const nav = document.querySelector("nav");
-window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
-    if(window.scrollY > 120){
-        nav.style.top = 0;
 
-}else{
-    nav.style.top ="-50px;";
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
 }
-});
+
 
 
 
