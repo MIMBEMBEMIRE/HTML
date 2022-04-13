a2b78f46d0c82cec72b9ef8684093f94c4d6e503
Mireille <miramimbembe1991@gmail.com>
2022-04-13 14:31:36 +0530

    create dable and insert row using mongo

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/Stoks", {
+    useNewUrlParser: "true"});
 
-mongoose.connect("mongodb://localhost:27017/Fruidb", {
-  useNewUrlParser: "true",
-});
-console.log("connected succesfully to server");
+console.log("suscess");
+const stokSchema = new mongoose.Schema({name:String,rating:Number,review:String});
 
-const fruitSchema = new mongoose.Schema({name:String,
-    rating:Number,
-    review:String,});
-    const Fruit = mongoose.model("Fruit",fruitSchema);
-    const fruit = new Fruit ({name:"apple",rating:7,review:"new fruit"});
-    fruit.save();
-    
+const prod = mongoose.model("produi",stokSchema);
+const prod1 = new prod({name:"mireille",rating:1,review:"aimer"});
+
+
+const personneShema = new mongoose.Schema({name:String,age:Number,addresse:String});
+const perso = mongoose.model("personnes", personneShema);
+const personna = new perso({name:"mirayessa",age:23,addresse:"chelikere"});
+personna.save();
