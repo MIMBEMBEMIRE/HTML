6d1d9a59b687cf3fd1aaf5604a98f4b38cf85a73
Mireille <miramimbembe1991@gmail.com>
2022-04-13 16:20:53 +0530

    mongoose validation give min and maxi

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ mongoose.connect("mongodb://localhost:27017/Stoks", {
     useNewUrlParser: "true"});
 
 console.log("suscess");
-const stokSchema = new mongoose.Schema({name:String,rating:Number,review:String});
+const stokSchema = new mongoose.Schema({ name:String, rating:{type:Number,min:1,max:10}, review:String});
 
-const prod = mongoose.model("produi",stokSchema);
-const prod1 = new prod({name:"mireille",rating:1,review:"aimer"});
+const prod = mongoose.model("produi",stokSchema)
+const prod1 = new prod({name:"mireille",rating:4,review:"aimer"});
 
 
 const personneShema = new mongoose.Schema({name:String,age:Number,addresse:String});
 const perso = mongoose.model("personnes", personneShema);
-const personna = new perso({name:"mirayessa",age:23,addresse:"chelikere"});
+const personna = new perso({name:"mirayessa",age:7,addresse:"chelikere"});
 
-const personel = new perso({name:"mimbembe",age:33,addresse:"kananga",});
+const personel = new perso({name:"mimbembe",age:2,addresse:"kananga",});
 
-const personne1 = new perso({name:"mim",age:42,addresse:"kananga",});
+const personne1 = new perso({name:"mim",age:4,addresse:"kananga",});
 
 perso.insertMany([personna,personel,personne1,] ,function(err){
     if(err){
@@ -26,10 +26,3 @@ perso.insertMany([personna,personel,personne1,] ,function(err){
     }
 
 });
-perso.find(function(erro, personnes){
-    if(erro){
-        console.log(erro);
-    }else{
-        console.log(personnes);
-    }
-})
