ea5ff2a3e41c1c447227bfd7be9200ff5b974587
Mireille <miramimbembe1991@gmail.com>
2022-04-13 14:58:15 +0530

    inserter many row to the table

--- app.js
+++ app.js
@@ -13,4 +13,17 @@ const prod1 = new prod({name:"mireille",rating:1,review:"aimer"});
 const personneShema = new mongoose.Schema({name:String,age:Number,addresse:String});
 const perso = mongoose.model("personnes", personneShema);
 const personna = new perso({name:"mirayessa",age:23,addresse:"chelikere"});
-personna.save();
+
+const personel = new perso({name:"mimbembe",age:33,addresse:"kananga",});
+
+const personne1 = new perso({name:"mim",age:42,addresse:"kananga",});
+
+perso.insertMany([personna,personel,personne1,] ,function(err){
+    if(err){
+        console.log("err")
+    }else{
+        console.log("succesfully");
+    }
+
+});
+
