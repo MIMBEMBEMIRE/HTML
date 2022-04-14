5b6e8d646af80b1c94f8bef79fcbe2a0b0b8257e
Mireille <miramimbembe1991@gmail.com>
2022-04-14 14:27:36 +0530

    display the day of the week with object

--- app.js
+++ app.js
@@ -8,40 +8,13 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res){
     
     var today = new Date();
-    var currentDay = today.getDay();
-    var day=""
-    switch(currentDay){
-        case 0:
-            day="DIMANCHE";
-            break;
-            case 1:
-                d="LUNDI";
-                break;
-                case 2:
-                    day="MARDI";
-                    break;
-                    case 3:
-                        day="MERCREDI";
-                        break;
-                        case 4:
-                            day="jeudi";
-                            break;
-                            case 5:
-                                day="vendredi";
-                                break;
-                                case 6:
-                                    day:"samedi"
-                                    break;
-                                    default:
-
-            
-
-                        
-
-    }
-
-
-    res.render("list",{wday:day})
+    var options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    },
+    day=today.toLocaleDateString("en-us",options);
+    res.render("list",{weekedly:day})
 
 });
 
