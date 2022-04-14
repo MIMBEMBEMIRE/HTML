581945afc2f333d4fb5559e7fc50b680736cdb01
Mireille <miramimbembe1991@gmail.com>
2022-04-14 13:54:22 +0530

    display day of week ejs

--- app.js
+++ app.js
@@ -10,12 +10,38 @@ app.get("/", function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
     var day=""
-    if(currentDay === 6 || currentDay === 0){
-    day="wesnesday"
-    res.render("list",{wday:day});
-    }else{
-        res.render("list",{wday :day});
+    switch(currentDay){
+        case 0:
+            day="DIMANCHE";
+            break;
+            case 1:
+                d="LUNDI";
+                break;
+                case 2:
+                    day="MARDI";
+                    break;
+                    case 3:
+                        day="MERCREDI";
+                        break;
+                        case 4:
+                            day="jeudi";
+                            break;
+                            case 5:
+                                day="vendredi";
+                                break;
+                                case 6:
+                                    day:"samedi"
+                                    break;
+                                    default:
+
+            
+
+                        
+
     }
+
+
+    res.render("list",{wday:day})
 
 });
 
