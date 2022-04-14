1fe26a6adb2ebee3af3a933b7b545dd7b31858a7
Mireille <miramimbembe1991@gmail.com>
2022-04-14 12:20:55 +0530

    how to get a days of week

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/livraison",{useNewUrlParser: true});
-console.log("server connected successfully");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app =express();
 
-const livraisonSchema = new mongoose.Schema({name:String,time:Number,periode:String});
-const Produit = new mongoose.model("produit", livraisonSchema);
-const pro2 = new Produit({name:"banane",time:2,periode:"mars"});
-
-const pro3 = new Produit({name:"potatos",time:4,perode:"mais"});
-const pro4  = new Produit({name:"alcol",time:5,perode:"avril"})
-
-Produit.insertMany([pro2,pro3,pro4],function(err){
-    if(err){
-        console.log("err");
-    }else{
-        console.log("success");
+app.get("/", function(req, res){
+    
+    var today = new Date();
+    if(today.getDay() === 6 || today.getDay() === 0){
+        res.send("tday is a weeks");
+    } else{
+        res.send("toda is a working days");
     }
 });
-Produit.find(function(err){
-    if(err){
-        console.log(err)
-    }else{
-        console.log(pro2.name)
-        console.log(pro3.time);
-    }
+    
+
+
+
+
+app.listen(3000,function(){
+    console.log("the server is running sucess full")
 });
