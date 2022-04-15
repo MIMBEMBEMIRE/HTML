af2881833616e8a99c48553ec682ce524db8af33
Mireille <miramimbembe1991@gmail.com>
2022-04-15 12:08:08 +0530

    how to add the data in the webside using mongooese

--- app.js
+++ app.js
@@ -1,51 +1,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const req = require("express/lib/request");
+const mongoose = require("mongoose")
 const app =express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 app.set('view engine', 'ejs');
-var items=["food","manag","soudi"];
-var item="";
-var workitems=[];
-app.get("/", function(req, res){
-    
-    var today = new Date();
-    var options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    },
-    day=today.toLocaleDateString("en-us",options);
-    res.render("list",{weekedly:day,newlistitem:items})
 
+
+mongoose.connect("mongodb://localhost:27017/dotolistdb",{useNewUrlParser: true});
+const itemSchema = {name:String}
+const Item = mongoose.model("Items",itemSchema);
+const item1 = new Item({name: "welcome to todo list"});
+const item2 = new Item({name: "hit the + button to add a new item"});
+const item3 = new Item({name: "hit this to delle an item"});
+
+const defautItem=[item1,item2,item3];
+/*Item.insertMany(defautItem,function(err){
+    if(err){
+        
+        console.log(err);
+    }else{
+        console.log("mongo is susscefull");
+    }
+});*/
+app.get("/",function(req, res){
+
+Item.find({},function(err, foundItems){
+    if(foundItems.length ===0){
+
+        Item.insertMany(defautItem,function(err){
+            if(err){
+                
+                console.log(err);
+            }else{
+                console.log("mongo is susscefull");
+            }
+            res.redirect("/")
+        });
+    }else{
+
+    res.render("list",{listTitle:"today",newlistitem:foundItems});
+    }
 });
+
 app.post("/",function(req, res){
-    item=req.body.newItem;
-   items.push(item);
-    
+    const ItemeName = req.body.newItem;
+    const item = new Item({name:ItemeName
+    });
+    item.save();
     res.redirect("/");
+});
 
 });
-    
 
-app.get("/work",function(req,res){
-
-res.render("list",{weekedly:"worklist",newlistitem:workitems});
-})
-app.post("/work",function(req, res){
-let item = req.body.newItem
-if(req.body.valida ==="work"){
-    workitems.push(item);
-    res.redirect("/work");
-}else{
-    item.push(item);
-}
-    
-    res.redirect("/")
-})
 app.listen(3000,function(){
     console.log("the server is running sucess full")
 });
