fe3272d1774bcaf453ddf1ee33abf37aa4f5bf8e
Mireille <miramimbembe1991@gmail.com>
2022-04-16 14:31:05 +0530

    insert row in web page without duplcation

--- app.js
+++ app.js
@@ -1,103 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const res = require("express/lib/response");
-const app =express();
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
-
-mongoose.connect("mongodb://localhost:27017/dotolistdb",{useNewUrlParser: true});
-const itemSchema = {name:String}
-const Item = mongoose.model("Items",itemSchema);
-const item1 = new Item({name: "welcome to todo list"});
-const item2 = new Item({name: "hit the + button to add a new item"});
-const item3 = new Item({name: "hit this to delle an item"});
-
-const defautItem=[item1,item2,item3];
-const listSchema={
+mongoose.connect("mongodb://localhost:27017/todolistDB1",{useNewUrlParser: true});
+const itemSchema = {
     name:String,
-    items:[itemSchema],
-};
-const List = mongoose.model("ELEVE",listSchema);
+}
+const Item = new mongoose.model("Item10",itemSchema);
+const item1 = new Item({name:"welcomw to mongo"});
+const item2 = new Item ({name:"thank you very much"});
+const item3 = new Item({name:"thank you every one "})
+const defaultItem =[item1,item2, item3];
 
 app.get("/",function(req, res){
 
-Item.find({},function(err, foundItems){
-    if(foundItems.length ===0){
-
-        Item.insertMany(defautItem,function(err){
-            if(err){
-                
-                console.log(err);
-            }else{
-                console.log("mongo is susscefull");
-            }
-            res.redirect("/")
-        });
-    }else{
-        res.render("list",{listTitle:"",newlistitem:foundItems});
-
-    }
-});
-
-});
-
-
-app.get("/:customListName",function(req, res){
-    const customListName=req.params.customListName;
-    List.findOne({name: customListName},function(err, foundList){
-        if(!err){
-            if(!foundList){
-                const list= new List({name:customListName,items:defautItem});   
-                 list.save();
-
-
-            }else{
-                res.render("list",{listTitle:foundList.name,newlistitem:foundList.items});
-
-            }
-            }
-            
-    });
+    Item.find({},function(err, foundItems){
+        if(foundItems.length ===0){
     
+            Item.insertMany(defaultItem,function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("mongo is susscefull");
+                }
+                res.redirect("/")
+            });
+        }else{
+            res.render("list",{listTitle:"Today",newlist1:foundItems});
     
-
-});
-
-
-app.post("/",function(req, res){
-    const ItemeName = req.body.newItem;
-    const listName=req.body.list;
-    const item = new Item({name:ItemeName
-    });
-
-    if(listName==="today"){
-        item.save();
-    res.redirect("/");
-}else{
-    List.findOne({name:listName,},function(err,foundList){
-        foundList.items.push(item);
-        foundList.save();
-        res.redirect("/" +listName);
-
-    });
-}
-    
-});
-app.post("/delete",function(req,res){
-    const check= req.body.checkox;
-    Item.findByIdAndRemove(check,function(err){
-        if(err){
-            console.log("succefuly deleted");
-
         }
-        res.redirect("/");
-    })
+    });
 
-})
+});    
+   
+
+
+
+
+
+                
+
+    
+
+
+
+
+
+
 
 
 app.listen(3000,function(){
